adff125086d7075f5e89fb634beee87c0c84c4b0
Javier García <javier_g@pretia.co.jp>
2024-01-25 14:39:50 +0900

    Initial Commit

---
+++ Memo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by Francisco Javier García Gutiérrez on 2024/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by Francisco Javier García Gutiérrez on 2024/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
